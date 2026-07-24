2d3b3a61978dc731a5517aeecacf9166f2f5fb2f
Karan <karan@example.com>
2026-07-25 00:12:10 +0530

    cloud storage integrated

--- backend/cloudConfig.js
+++ backend/cloudConfig.js
@@ -1,5 +1,5 @@
-const cloudinary = require("cloudinary").v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -15,7 +15,4 @@ const storage = new CloudinaryStorage({
   },
 });
 
-module.exports = {
-  cloudinary,
-  storage,
-};
+export { cloudinary, storage };
